65c86fafdaf19641e2debfd1defd17d6d3faa8b5
12zehan17 <365486572@qq.com>
2022-10-05 19:41:44 +0800

    masks load reconstruct module

---
+++ lib/loss_helper/loss_reconstruct.py
@@ -0,0 +1,47 @@
+import torch
+
+
+def reconstruct_loss(logit, idx, mask, weights=None):
+    '''
+        logit: [bs, len_num_max, max_des_len, vocab_size]
+        idx: [bs, len_num_max, max_des_len]
+        mask: [bs, len_num_max, max_des_len]
+    '''
+
+    bs, len_num_max, max_des_len = logit.shape[:3]
+    logit = logit.reshape(bs * len_num_max, max_des_len, -1)
+    idx = idx.reshape(bs * len_num_max, max_des_len)
+    mask = mask.reshape(bs * len_num_max, max_des_len)
+
+    eps = 0.1
+    logit = logit.log_softmax(dim=-1)
+    nll_loss = -logit.gather(dim=-1, index=idx.unsqueeze(-1)).squeeze(-1)  # [bs * len_num_max, max_des_len]
+    smooth_loss = -logit.sum(dim=-1)  # [bs * len_num_max, max_des_len]
+    nll_loss = (1 - eps) * nll_loss + eps / logit.size(-1) * smooth_loss
+
+    nll_loss = nll_loss.masked_fill(mask == 0, 0)
+    nll_loss = nll_loss.masked_fill(mask == 2, 0)
+    nll_loss = nll_loss.sum(dim=-1) / (mask==1).int().sum(dim=-1)  # [bs * len_num_max]
+    # nll_loss = nll_loss.mean()
+    return nll_loss.contiguous()
+
+def weakly_supervised_loss(score, rec_loss):
+    '''
+        score: [bs*len_num_max, n_candidate]
+        rec_loss: [bs*len_num_max, n_candidate]
+    '''
+    n_candidate = score.shape[1]
+    rewards = torch.linspace(0,1,n_candidate)   # pseudo-label by rec_loss
+
+    idx = torch.argsort(rec_loss, dim=-1, descending=True)
+    _, idx = torch.sort(idx, dim=-1)
+    rewards = rewards[idx]
+    grounding_loss = -(rewards * score.log_softmax(dim=-1))
+    return grounding_loss
+
+
+if __name__ == '__main__':
+    score = torch.rand(10, 5)
+    rec_loss = torch.rand(10, 5)
+    print(rec_loss)
+    weakly_supervised_loss(score, rec_loss)
--- models/recnet/reconstruct_module.py
+++ models/recnet/reconstruct_module.py
@@ -20,21 +20,24 @@ class ReconstructModule(nn.Module):
                                              num_decoder_layers=num_decoder_layers, dropout=0.05)
         self.vocab_fc = nn.Linear(hidden_size, vocab_size)
 
-    def forward(self, original_embs, object_feat, data_dict):
+    def forward(self, original_embs, object_feat, masks_list):
         '''
             words_feat: [bs, len_num_max, max_des_len, emb_dim]
             object_feat: [bs, 1+proposal, dim]
             data_dict['XXX_masks_list']: [bs, len_num_max, max_des_len]
         '''
-        batch_size, len_nun_max, max_des_len = original_embs.shape[:3]
+        batch_size, len_num_max, max_des_len = original_embs.shape[:3]
         batch_size, object_num = object_feat.shape[:2]
-        word_embs = original_embs.reshape(batch_size * len_nun_max, max_des_len, -1)
-        all_masks_list = data_dict['all_masks_list'].reshape(batch_size * len_nun_max, max_des_len).repeat(1, 1, self.emb_size)
+
+        object_feat = object_feat[:, None, :, :].repeat(1, len_num_max, 1, 1).reshape(batch_size*len_num_max, object_num, -1)
+        word_embs = original_embs.reshape(batch_size * len_num_max, max_des_len, -1)
+        all_masks_list = masks_list.reshape(batch_size * len_num_max, max_des_len).unsqueeze(0).repeat(1, 1, self.emb_size)
         all_masked_embs = self._mask_words(word_embs, all_masks_list) + self.word_position
+
         embs_mask = 1 - (all_masks_list==2).int()
-        object_mask = torch.zeros(batch_size, object_num)
+        object_mask = torch.zeros(batch_size*len_num_max, object_num)
         trans_out = self.reconstruct_trans(all_masked_embs, embs_mask, object_feat, object_mask)
-        word_logit = self.word_fc(trans_out)
+        word_logit = self.word_fc(trans_out).reshape(batch_size, len_num_max, max_des_len, -1)  #[bs, len_num_max, max_des_len, vocab_size]
         return word_logit
 
     def _mask_words(self, words_feat, mask_list):
@@ -48,7 +51,7 @@ class ReconstructModule(nn.Module):
         masked_words = mask_list.unsqueeze(-1).repeat(1, 1, self.emb_size)
         # exit(0)
         masked_words_vec = words_feat.new_zeros(*words_feat.size()) + token
-        masked_words_vec = masked_words_vec.masked_fill_(masked_words != 1, 0)
+        masked_words_vec = masked_words_vec.masked_fill(masked_words != 1, 0)
         words_feat1 = words_feat.masked_fill(masked_words == 1, 0) + masked_words_vec
         return words_feat1
 
